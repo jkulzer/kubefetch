6e986da1aca42e62f6c6ad73452e64643760dd3f
jkulzer <kulzer.johannes@tutanota.com>
2023-08-17 17:52:17 +0200

    fixed typo

--- kubefetch.go
+++ kubefetch.go
@@ -18,10 +18,10 @@ import (
 	"embed"
 	"io/fs"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+corev1 "k8s.io/api/core/v1"
+metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+"k8s.io/client-go/kubernetes"
 
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
